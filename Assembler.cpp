25c50a361a0c485302ac7c5692fb743495aed1f0
Jerryyyx <72396984+Jerryyyx@users.noreply.github.com>
2021-12-04 18:37:50 +0000

    Added convertToBinaryVector function
    added the function that will take the uncommentedCode and turn it to binary code as a vector of array of ints.

--- Assembler.cpp
+++ Assembler.cpp
@@ -88,7 +88,122 @@ int  Assembler::DecimalToBinary(int dec) {
     return std::stoi(s);
 }
 
+vector<array<int, 32>> Assembler::convertToBinaryVector(vector<string> uncommentedCode) {
+    vector<array<int, 32>> BinaryCode;
+    vector<pair<string,int>> variables;
+    int vars = 0;
+    
+    //Counts how many variables there are
+    for(int i=1; i < uncommentedCode.size(); i++){
+        if (uncommentedCode[i].find("VAR") != std::string::npos){
+            vars++;
+            variables.push_back(std::make_pair(uncommentedCode[i].substr(0, uncommentedCode[i].find("VAR") - 5),i));
+        }
+    }
+    
+    //Code to put variables at the bottom of the binary code
+    for(int i = 32-vars; i < 32; i++){
+        int index = uncommentedCode.size() - vars;
+        bool negative = false;
+        int num = std::stoi(uncommentedCode[index].substr(uncommentedCode[index].find("VAR") + 2));
 
+        int binaryNum[32] = { 0 };
+        
+        if (num < 0){
+            num *= -1;
+            negative = true;
+        }
+    
+        int x = 0;
+        while (num > 0) {
+            binaryNum[x] = num % 2;
+            num /= 2;
+            x++;
+        }   
+        
+        for (int z = 0; z < 32; z++){
+            if (binaryNum[z] == 1){
+                binaryNum[z] = 0;
+            }
+            else if(binaryNum[z] == 0){
+                binaryNum[z] = 1;
+            }
+        }
+
+        if (binaryNum[31] == 0){
+            binaryNum[31] = 1;
+        }
+        else if(binaryNum[31] == 1){
+            binaryNum[31] = 0;
+        }
+
+        for (int y = 0; y < 32; y++){
+            BinaryCode[i][y] = binaryNum[y];
+        }
+    }
+
+    //Sets the 13,14,15 slots in the 32bit binary according to the command in the machine code
+    for(int i=1; i < uncommentedCode.size() - vars; i++){
+        if (uncommentedCode[i].find("JMP") != std::string::npos){
+            BinaryCode[i][13] = 0;
+            BinaryCode[i][14] = 0;
+            BinaryCode[i][15] = 0;
+        }
+        else if(uncommentedCode[i].find("JRP") != std::string::npos){
+            BinaryCode[i][13] = 1;
+            BinaryCode[i][14] = 0;
+            BinaryCode[i][15] = 0;
+        }
+        else if(uncommentedCode[i].find("LDN") != std::string::npos){
+            BinaryCode[i][13] = 0;
+            BinaryCode[i][14] = 1;
+            BinaryCode[i][15] = 0;
+        }
+        else if(uncommentedCode[i].find("STO") != std::string::npos){
+            BinaryCode[i][13] = 1;
+            BinaryCode[i][14] = 1;
+            BinaryCode[i][15] = 0;
+        }
+        else if(uncommentedCode[i].find("SUB") != std::string::npos){
+            BinaryCode[i][13] = 0;
+            BinaryCode[i][14] = 0;
+            BinaryCode[i][15] = 1;
+        }
+        else if(uncommentedCode[i].find("CMP") != std::string::npos){
+            BinaryCode[i][13] = 0;
+            BinaryCode[i][14] = 1;
+            BinaryCode[i][15] = 1;
+        }
+        else if(uncommentedCode[i].find("STP") != std::string::npos){
+            BinaryCode[i][13] = 1;
+            BinaryCode[i][14] = 1;
+            BinaryCode[i][15] = 1;
+        }
+        else{
+            throw std::invalid_argument("Bad assembly code");
+        }
+
+        //Sets the 0,1,2,3,4 slots depending on the variables referenced in the machine code
+        for (int y = 0; y < variables.size(); y++){
+            if (uncommentedCode[i].find(variables[y].first) != std::string::npos){
+                int decimal = variables[y].second;
+                int position[5] = {0};
+                int z = 0;
+                while (decimal > 0) {
+                    position[z] = decimal % 2;
+                    decimal /= 2;
+                    z++;
+                }
+
+                for (int m = 0; m < 5; m++){
+                    BinaryCode[i][m] = position[m];
+                }
+            }
+        }
+    }
+
+    return BinaryCode;
+}
 
 
 
